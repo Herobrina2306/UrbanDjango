26aabc3fbb334393ada0f12087ba2f5786e2e550
AleksandraM <musatova.aleksandra@mail.ru>
2025-01-07 20:14:24 +0300

    module_18_2

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task2.views import class_django, func_django
+
+#На красное подчёркивание пути не обращать внимания!!! Оно работает!!!
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', class_django),
+    path('func/', func_django),
 ]
